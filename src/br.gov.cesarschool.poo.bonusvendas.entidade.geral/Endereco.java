4d5b388beea2205bda86e8c9332d8c2e7187f506
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-09-28 09:03:41 -0300

    Create Endereco.java

---
+++ src/br.gov.cesarschool.poo.bonusvendas.entidade.geral/Endereco.java
@@ -0,0 +1,80 @@
+package br.gov.cesarschool.poo.bonusvendas.entidade.geral;
+
+public class Endereco {
+	private String logradouro;
+	private int numero;
+	private String complemento;
+	private String cep;
+	private String cidade;
+	private String estado;
+	private String pais;
+
+	public Endereco(String logradouro, int numero, String complemento, String cep, String cidade, String estado,
+			String pais) {
+		super();
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.cep = cep;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.pais = pais;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+}
