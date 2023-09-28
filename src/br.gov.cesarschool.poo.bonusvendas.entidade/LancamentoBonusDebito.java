3100516487f9529841abd7ee0a53f7a7a104ca54
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-09-28 09:07:23 -0300

    Create LancamentoBonusDebito.java

---
+++ src/br.gov.cesarschool.poo.bonusvendas.entidade/LancamentoBonusDebito.java
@@ -0,0 +1,17 @@
+package br.gov.cesarschool.poo.bonusvendas.entidade;
+
+import java.time.LocalDateTime;
+
+public class LancamentoBonusDebito extends LancamentoBonus {
+
+	private TipoResgate tipo;
+
+	public LancamentoBonusDebito(long numeroCaixaDeBonus, int valor, LocalDateTime dataHoraLancamento) {
+		super(numeroCaixaDeBonus, valor, dataHoraLancamento);
+	}
+
+	public TipoResgate getTipo() {
+		return tipo;
+	}
+
+}
