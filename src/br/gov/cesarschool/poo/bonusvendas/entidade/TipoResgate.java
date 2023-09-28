04720fa1837a4a7c06cdfc8fa9f964ff0eddf7b0
paulo <paulo.m.campos6601@gmail.com>
2023-09-28 09:44:31 -0300

    reformulaçao do sistema

---
+++ src/br/gov/cesarschool/poo/bonusvendas/entidade/TipoResgate.java
@@ -0,0 +1,21 @@
+package br.gov.cesarschool.poo.bonusvendas.entidade;
+
+public enum TipoResgate {
+	PRODUTO(1, "produto"), SERVICO(2, "serviço"), CASH(3, "cash");
+
+	private final int codigo;
+	private final String descricao;
+
+	private TipoResgate(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
