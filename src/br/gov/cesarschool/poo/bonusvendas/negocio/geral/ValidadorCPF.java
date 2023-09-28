d6abf5840c7be3e242965c1e0cd73c1788f762f4
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2023-09-28 20:33:53 -0300

    Finalizando a classe ValidadorCpf

--- src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/ValidadorCPF.java
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/ValidadorCPF.java
@@ -1,21 +1,21 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 
 public class ValidadorCPF {
-	
+
 	private ValidadorCPF() {
-		
+
 	}
-	
+
 	public static boolean ehCpfValidado(String cpf) {
 		int tamanho = cpf.length();
-		
-		//conferindo se todos os digitos deles são numeros
-		//E se são 11 caracters
+
+		// conferindo se todos os digitos deles são numeros
+		// E se são 11 caracters
 		if (cpf.matches("[0-9]+") && tamanho == 11) {
 			/**
 			 * TODO usar um algoritmo para saber se o cpf é verdadeiro
-			 * */
-			
+			 */
+
 			return true;
 		} else {
 			return false;
