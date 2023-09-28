bc59b545a40527589d0a53734c1c05662966ef7f
paulo <paulo.m.campos6601@gmail.com>
2023-09-28 10:10:56 -0300

    inserçao de metodos na classe StringUtil

--- src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
@@ -2,4 +2,18 @@ package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 
 public class StringUtil {
 
+	private StringUtil() {
+
+	}
+
+	public static boolean ehNuloOuBranco(String str) {
+		if (str == null) {
+			return true;
+		} else {
+			/*
+			 * TODO verificar implementação de acordo com questão
+			 */
+			return false;
+		}
+	}
 }
