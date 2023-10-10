b05ee8458a793fe1e5e1a03fdb85026a1c097f4b
dan-albuquerque <daniloalbuquerquemelo@gmail.com>
2023-10-10 11:54:31 -0300

    fazendo vendedordao

--- src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
@@ -1,7 +1,46 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
+import java.io.Serializable;
+
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
 
 public class VendedorDAO {
+	private static final String BRANCO = "";
+	private CadastroObjetos cadastro = new CadastroObjetos(Vendedor.class);
 
+	public boolean incluir(Vendedor prod) {
+		Vendedor prodBusca = buscar(prod.getCpf());
+		if (prodBusca != null) {
+			return false;
+		} else {
+			cadastro.incluir(prod, BRANCO + prod.getCpf());
+			return true;
+		}
+	}
+
+	public boolean alterar(Vendedor prod) {
+		Vendedor prodBusca = buscar(prod.getCpf());
+		if (prodBusca == null) {
+			return false;
+		} else {
+			cadastro.alterar(prod, BRANCO + prod.getCpf());
+			return true;
+		}
+	}
+
+	public Vendedor buscar(String string) {
+		// Esta operação entre () vai ter significado mais à frente!
+		return (Vendedor) cadastro.buscar(BRANCO + string);
+	}
+
+	public Vendedor[] buscarTodos() {
+		Serializable[] rets = cadastro.buscarTodos(Vendedor.class);
+		Vendedor[] prods = new Vendedor[rets.length];
+		for (int i = 0; i < rets.length; i++) {
+			// Esta operação entre () vai ter significado mais à frente!
+			prods[i] = (Vendedor) rets[i];
+		}
+		return prods;
+	}
 }
